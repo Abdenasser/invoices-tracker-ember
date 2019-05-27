5154fd3150be1c591916e4ad9335ace5dcf1e6f8
Abdenasser <nasser.elidrissi065@gmail.com>
2019-05-27 01:15:50 +0000

    configuring mirage to return invoices at /api/invoices as a json-api

--- mirage/config.js
+++ mirage/config.js
@@ -1,26 +1,33 @@
 export default function() {
+  this.namespace = '/api';
 
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.4.x/shorthands/
-  */
+  this.get('/invoices', function() {
+    return {
+      data: [{
+        type: 'invoices',
+        id: 'inv1',
+        attributes: {
+          title: 'invoice 1',
+          amount: '200',
+          date:'13/02/2019'
+        }
+      }, {
+        type: 'invoices',
+        id: 'inv2',
+        attributes: {
+          title: 'invoice 2',
+          amount: '150',
+          date: '02/03/2019'
+        }
+      }, {
+        type: 'invoices',
+        id: 'inv3',
+        attributes: {
+          title: 'invoice 3',
+          amount: '1050',
+          date: '26/04/2019'
+        }
+      }]
+    };
+  });
 }
