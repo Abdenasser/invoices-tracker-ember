de7c20730bee8dcd889c8ad8c924aa2336068fa2
Abdenasser <nasser.elidrissi065@gmail.com>
2019-05-27 02:05:37 +0000

    added filterByDate action to invoices controller

--- app/controllers/invoices.js
+++ app/controllers/invoices.js
@@ -1,4 +1,13 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    filterByDate(param) {
+      if (param !== '') {
+        return this.store.query('invoice', { date: param });
+      } else {
+        return this.store.findAll('invoice');
+      }
+    }
+  }
 });
