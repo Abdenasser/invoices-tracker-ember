ce86e21ea33c8374dfd1d8b942744e7236cb5bc0
Abdenasser <nasser.elidrissi065@gmail.com>
2019-05-27 02:15:07 +0000

    updated mirage to start returning filtered invoices

--- mirage/config.js
+++ mirage/config.js
@@ -1,33 +1,40 @@
 export default function() {
   this.namespace = '/api';
 
-  this.get('/invoices', function() {
-    return {
-      data: [{
-        type: 'invoices',
-        id: 'inv1',
-        attributes: {
-          title: 'invoice 1',
-          amount: '200',
-          date:'13/02/2019'
-        }
-      }, {
-        type: 'invoices',
-        id: 'inv2',
-        attributes: {
-          title: 'invoice 2',
-          amount: '150',
-          date: '02/03/2019'
-        }
-      }, {
-        type: 'invoices',
-        id: 'inv3',
-        attributes: {
-          title: 'invoice 3',
-          amount: '1050',
-          date: '26/04/2019'
-        }
-      }]
-    };
+  let invoices = [{       
+    type: 'invoices',
+    id: 'inv1',
+    attributes: {
+      title: 'invoice 1',
+      amount: '200',
+      date:'13/02/2019'
+    }
+  }, {
+    type: 'invoices',
+    id: 'inv2',
+    attributes: {
+      title: 'invoice 2',
+      amount: '150',
+      date: '02/03/2019'
+    }
+  }, {
+    type: 'invoices',
+    id: 'inv3',
+    attributes: {
+      title: 'invoice 3',
+      amount: '1050',
+      date: '26/04/2019'
+    }
+  }]
+
+  this.get('/invoices', function(db, request) {
+    if(request.queryParams.date !== undefined) {
+      let filteredInvoices = invoices.filter(function(i) {
+        return i.attributes.date.indexOf(request.queryParams.date) !== -1;
+      });
+      return { data: filteredInvoices };
+    } else {
+      return { data: invoices };
+    }
   });
 }
