bbc095b01446a31c4f5d0101d66c39dc5343a03d
Abdenasser <nasser.elidrissi065@gmail.com>
2019-05-27 01:00:22 +0000

    added some hardcoded invoices to feed our template

--- app/routes/invoices.js
+++ app/routes/invoices.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [{
+      id: 'inv1',
+      title: 'invoice 1',
+      amount: '200',
+      date:'13/02/2019'
+    }, {
+      id: 'inv2',
+      title: 'invoice 2',
+      amount: '150',
+      date: '02/03/2019'
+    }, {
+      id: 'inv3',
+      title: 'invoice 3',
+      amount: '1050',
+      date: '26/04/2019'
+    }];
+  }
 });
