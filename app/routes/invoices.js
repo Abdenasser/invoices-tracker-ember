187dde99afccbbcfac265720ab925b29e5862cee
Abdenasser <nasser.elidrissi065@gmail.com>
2019-05-27 01:42:22 +0000

    using ember data model instead of hardcoded data to fetch our invoice list

--- app/routes/invoices.js
+++ app/routes/invoices.js
@@ -2,21 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return [{
-      id: 'inv1',
-      title: 'invoice 1',
-      amount: '200',
-      date:'13/02/2019'
-    }, {
-      id: 'inv2',
-      title: 'invoice 2',
-      amount: '150',
-      date: '02/03/2019'
-    }, {
-      id: 'inv3',
-      title: 'invoice 3',
-      amount: '1050',
-      date: '26/04/2019'
-    }];
+    return this.store.findAll('invoice');
   }
 });
